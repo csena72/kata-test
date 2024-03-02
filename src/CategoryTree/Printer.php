95686067f431648ee606e411c5de26f45e3f9da4
Cristian Sena <cristianjsena@gmail.com>
2024-03-02 12:38:48 -0300

    Agrego solución a kata 2

--- src/CategoryTree/Printer.php
+++ src/CategoryTree/Printer.php
@@ -6,9 +6,30 @@ class Printer
 {
     public function build(Category $parent, int $spaces = 0): string
     {
-        /**
-         * @todo
-         */
-        return '';
+        $result = $this->buildTree($parent, $spaces);
+        
+        return $result;
     }
+    
+    private function buildTree(Category $category, int $spaces): string
+    {
+        $tree = '';
+            
+        $indentation = str_repeat('  ', $spaces);
+        
+        $tree .= $indentation . $category->name() . PHP_EOL;
+        
+        $sortedChildren = $category->children();
+
+        usort($sortedChildren, function(Category $a, Category $b) {
+            return $a->name() <=> $b->name();
+        });
+    
+    
+        foreach ($sortedChildren as $child) {
+            $tree .= $this->buildTree($child, $spaces + 1);
+        }
+    
+        return $tree;
+    }    
 }
